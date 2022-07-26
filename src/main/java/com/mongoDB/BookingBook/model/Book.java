5559234e025981ef886587d374c705d6e9afe126
Alireza Valikhani <alirezavalikhabi1998@gmail.com>
2022-07-26 11:55:41 +0430

    Models added to project

---
+++ src/main/java/com/mongoDB/BookingBook/model/Book.java
@@ -0,0 +1,23 @@
+package com.mongoDB.BookingBook.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Document(collection = "book")
+public class Book {
+    @Id
+    private String id;
+    private String name;
+    private String authorName;
+    private Boolean isReserve;
+    private User user;
+    private Library library;
+}
---
+++ src/main/java/com/mongoDB/BookingBook/model/User.java
@@ -0,0 +1,25 @@
+package com.mongoDB.BookingBook.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Document(collection = "user")
+public class User {
+    @Id
+    private String id;
+    private String fullName;
+    private String nationalCode;
+    private String email;
+    private List<Book> books;
+    private Library library;
+}
