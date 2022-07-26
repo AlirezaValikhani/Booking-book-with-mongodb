917fbf32e6dcb46d359a2d1d88bc689ebaed0330
Alireza Valikhani <alirezavalikhabi1998@gmail.com>
2022-07-26 11:13:47 +0430

    Built project

---
+++ src/main/java/com/mongoDB/BookingBook/BookingBookApplication.java
@@ -0,0 +1,13 @@
+package com.mongoDB.BookingBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mongoDB/BookingBook/BookingBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mongoDB.BookingBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
